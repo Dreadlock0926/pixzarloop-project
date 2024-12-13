32e3b2edfa70af757dde9cd178536534d7c9dbc4
Dreadlock0926 <105129406+Dreadlock0926@users.noreply.github.com>
2024-12-14 03:38:30 +0530

    added react-icons; created base for borrowings;

--- part-2/src/pages/Borrowings.jsx
+++ part-2/src/pages/Borrowings.jsx
@@ -1,12 +1,60 @@
 import Navbar from "../components/Navbar";
+import "./Borrowings.css";
+import { FaCheck } from "react-icons/fa";
 
 function Borrowings() {
 
+  function BorrowingsTable({borrowingsType}) {
+
+    return (
+      <div className="current-borrowings-container">
+        <h2 className="current-borrowings-title">{borrowingsType} Borrowings</h2>
+        <table className="current-borrowings">
+          <thead>
+            <tr>
+              <th>Book</th>
+              <th>ISBN</th>
+              <th>Due Date</th>
+              <th>Borrower</th>
+              {borrowingsType !== "Returned" && <th>Actions</th>}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Book 1</td>
+              <td>123456789</td>
+              <td>2022-12-31</td>
+              <td>John Doe</td>
+              {borrowingsType !== "Returned" && <td title="Book returned"><FaCheck/></td>}
+            </tr>
+            <tr>
+              <td>Book 2</td>
+              <td>987654321</td>
+              <td>2022-12-31</td>
+              <td>Jane Doe</td>
+              {borrowingsType !== "Returned" && <td title="Book returned"><FaCheck/></td>}
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    )
+
+  }
+
   return (
-    <div>
-      <Navbar />
-      <h1>Dashboard</h1>
-    </div>
+    <>
+
+      <header>
+        <Navbar />
+      </header>
+
+      <main>
+        <BorrowingsTable borrowingsType="Current" />
+        <BorrowingsTable borrowingsType="Overdue" />
+        <BorrowingsTable borrowingsType="Returned" />
+      </main>
+
+    </>
   );
   
 }
