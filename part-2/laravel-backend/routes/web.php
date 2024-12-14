c56c4e091add7da4b23baa7c80dadffc610225c2
Dreadlock0926 <105129406+Dreadlock0926@users.noreply.github.com>
2024-12-14 11:25:40 +0530

    Added roles get

--- part-2/laravel-backend/routes/web.php
+++ part-2/laravel-backend/routes/web.php
@@ -2,10 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\RoleController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Role routes
+Route::get('roles', [RoleController::class, 'index']);
+
+// Book routes
+
 Route::get('books', [BookController::class, 'index']);
 Route::get('books/{id}', [BookController::class, 'show']);
 Route::post('books', [BookController::class, 'store']);
