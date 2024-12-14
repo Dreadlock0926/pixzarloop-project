e76867e4a5a00d2009c559ca112b0f833735c4a3
Dreadlock0926 <105129406+Dreadlock0926@users.noreply.github.com>
2024-12-14 17:15:02 +0530

    added authorization to all routes

--- part-2/laravel-backend/routes/web.php
+++ part-2/laravel-backend/routes/web.php
@@ -26,22 +26,22 @@ Route::post('login', [AuthController::class, 'login']);
 Route::post('users', [UserController::class, 'store']);
 
 // Borrowing routes
-Route::get('borrowings', [BorrowingController::class, 'index']);
-Route::post('borrowings', [BorrowingController::class, 'store']);
-Route::put('borrowings/{borrow_id}', [BorrowingController::class, 'update']);
-Route::put('borrowings/{borrow_id}/return', [BorrowingController::class, 'markReturned']);
+Route::get('borrowings', [BorrowingController::class, 'index'])->middleware('auth:sanctum');
+Route::post('borrowings', [BorrowingController::class, 'store'])->middleware('auth:sanctum');
+Route::put('borrowings/{borrow_id}', [BorrowingController::class, 'update'])->middleware('auth:sanctum');
+Route::put('borrowings/{borrow_id}/return', [BorrowingController::class, 'markReturned'])->middleware('auth:sanctum');
 
 // Member routes
-Route::resource('members', MemberController::class);
+Route::resource('members', MemberController::class)->middleware('auth:sanctum');
 
 // Author routes
-Route::resource('authors', AuthorController::class);
+Route::resource('authors', AuthorController::class)->middleware('auth:sanctum');
 
 // Genre routes
-Route::resource('genres', GenreController::class)->middleware('auth:sanctum', ['only' => ['store']]);
+Route::resource('genres', GenreController::class)->middleware('auth:sanctum');
 
 // Role routes
-Route::resource('roles', RoleController::class);
+Route::resource('roles', RoleController::class)->middleware('auth:sanctum');
 
 // Book routes
-Route::resource('books', BookController::class);
+Route::resource('books', BookController::class)->middleware('auth:sanctum');
